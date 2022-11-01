075245ba0f9a921121956e20f4c075044e69a20b
Lucas Gabriel <lucas.ddd@hotmail.com>
2022-11-01 19:43:45 -0300

    Improve on image upload

--- resources/views/admin/pages/people/_partials/form.blade.php
+++ resources/views/admin/pages/people/_partials/form.blade.php
@@ -3,20 +3,21 @@
         <div class="row">
             <div class="card">
                 <div class="card-body">
-                    <div class="p-3">
+                    <div class="p-3" id="img-container">
                         @if(isset($person->image))
                             <img src="{{url("{$person->image}")}}"
-                            alt="" class="card-img-top">
+                            alt="" class="card-img-top" id="image-preview">
                         @endif
                         @if(!isset($person->image))
                             <img src="{{url('img/blank-profile.png')}}"
-                            alt="" class="card-img-top">
+                            alt="" class="card-img-top" id="image-preview">
                         @endif
 
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 text-center">
                         <label for="formFile" class="form-label">Foto</label>
-                        <input class="form-control" id="formFile" type="file" name="image">
+                        <input class="form-control" id="img-input" type="file" name="image" accept="image/*">
+                        <small>Inserir foto sem maquiagem, óculos de sol ou qualquer enfeite...</small>
                     </div>
                 </div>
             </div>
@@ -305,116 +306,130 @@
         <button type="submit" class="btn btn-dark">Salvar</button>
     </div>
 </div>
-<script>
-    @php
-    if(isset($person)){ @endphp
-        window.onload = function exampleFunction() {
-            const selected = document.querySelector('#state');
-            const value = "@php echo $person->state @endphp";
-            selected.value = value;
-
-            const is_baptized = @php echo $person->is_baptized @endphp;
-            if(is_baptized == 1){
-                const is_baptized_yes = document.querySelector('#is_baptized_yes');
-                is_baptized_yes.checked = true;
-            }else{
-                const is_baptized_no = document.querySelector('#is_baptized_no');
-                is_baptized_no.checked = true;
+@section('js')
+    <script>
+        function readImage() {
+            if (this.files && this.files[0]) {
+                var file = new FileReader();
+                file.onload = function(e) {
+                    document.getElementById("image-preview").src = e.target.result;
+                };
+                file.readAsDataURL(this.files[0]);
             }
+        }
+        document.getElementById("img-input").addEventListener("change", readImage, false);
 
-            const is_confirmed = @php echo $person->is_confirmed @endphp;
-            if(is_confirmed == 1){
-                const is_confirmed_yes = document.querySelector('#is_confirmed_yes');
-                is_confirmed_yes.checked = true;
-            }else{
-                const is_confirmed_no = document.querySelector('#is_confirmed_no');
-                is_confirmed_no.checked = true;
-            }
+        @php
+        if(isset($person)){ @endphp
+            window.onload = function exampleFunction() {
+                const selected = document.querySelector('#state');
+                const value = "@php echo $person->state @endphp";
+                selected.value = value;
 
-            const is_eucharist = @php echo $person->is_eucharist @endphp;
-            if(is_eucharist == 1){
-                const is_eucharist_yes = document.querySelector('#is_eucharist_yes');
-                is_eucharist_yes.checked = true;
-            }else{
-                const is_eucharist_no = document.querySelector('#is_eucharist_no');
-                is_eucharist_no.checked = true;
-            }
-
-            const is_pastoral = @php echo $person->is_pastoral @endphp;
-            if(is_pastoral == 1){
-                const is_pastoral_yes = document.querySelector('#is_pastoral_yes');
-                const pastoral = document.querySelector('#pastoral');
-                is_pastoral_yes.checked = true;
-                pastoral.disabled = false;
-            }else{
-                const is_pastoral_no = document.querySelector('#is_pastoral_no');
-                is_pastoral_no.checked = true;
-            }
-
-            const is_married = @php echo $person->is_married @endphp;
-            if(is_married == 1){
-                const is_married_yes = document.querySelector('#is_married_yes');
-                is_married_yes.checked = true;
-
-                const is_spouse_camper = @php
-                if($person->is_spouse_camper) echo $person->is_spouse_camper;
-                else echo "null";
-                @endphp;
-                const is_spouse_camper_yes = document.querySelector('#is_spouse_camper_yes');
-                const is_spouse_camper_no = document.querySelector('#is_spouse_camper_no');
-                is_spouse_camper_yes.disabled = false;
-                is_spouse_camper_no.disabled = false;
-
-                if(is_spouse_camper == 1){
-                    is_spouse_camper_yes.checked = true;
+                const is_baptized = @php echo $person->is_baptized @endphp;
+                if(is_baptized == 1){
+                    const is_baptized_yes = document.querySelector('#is_baptized_yes');
+                    is_baptized_yes.checked = true;
                 }else{
-                    is_spouse_camper_no.checked = true;
+                    const is_baptized_no = document.querySelector('#is_baptized_no');
+                    is_baptized_no.checked = true;
                 }
 
-            }else{
-                const is_married_no = document.querySelector('#is_married_no');
-                is_married_no.checked = true;
+                const is_confirmed = @php echo $person->is_confirmed @endphp;
+                if(is_confirmed == 1){
+                    const is_confirmed_yes = document.querySelector('#is_confirmed_yes');
+                    is_confirmed_yes.checked = true;
+                }else{
+                    const is_confirmed_no = document.querySelector('#is_confirmed_no');
+                    is_confirmed_no.checked = true;
+                }
+
+                const is_eucharist = @php echo $person->is_eucharist @endphp;
+                if(is_eucharist == 1){
+                    const is_eucharist_yes = document.querySelector('#is_eucharist_yes');
+                    is_eucharist_yes.checked = true;
+                }else{
+                    const is_eucharist_no = document.querySelector('#is_eucharist_no');
+                    is_eucharist_no.checked = true;
+                }
+
+                const is_pastoral = @php echo $person->is_pastoral @endphp;
+                if(is_pastoral == 1){
+                    const is_pastoral_yes = document.querySelector('#is_pastoral_yes');
+                    const pastoral = document.querySelector('#pastoral');
+                    is_pastoral_yes.checked = true;
+                    pastoral.disabled = false;
+                }else{
+                    const is_pastoral_no = document.querySelector('#is_pastoral_no');
+                    is_pastoral_no.checked = true;
+                }
+
+                const is_married = @php echo $person->is_married @endphp;
+                if(is_married == 1){
+                    const is_married_yes = document.querySelector('#is_married_yes');
+                    is_married_yes.checked = true;
+
+                    const is_spouse_camper = @php
+                    if($person->is_spouse_camper) echo $person->is_spouse_camper;
+                    else echo "null";
+                    @endphp;
+                    const is_spouse_camper_yes = document.querySelector('#is_spouse_camper_yes');
+                    const is_spouse_camper_no = document.querySelector('#is_spouse_camper_no');
+                    is_spouse_camper_yes.disabled = false;
+                    is_spouse_camper_no.disabled = false;
+
+                    if(is_spouse_camper == 1){
+                        is_spouse_camper_yes.checked = true;
+                    }else{
+                        is_spouse_camper_no.checked = true;
+                    }
+
+                }else{
+                    const is_married_no = document.querySelector('#is_married_no');
+                    is_married_no.checked = true;
+                }
+            }
+        @php } @endphp
+
+        function formatar(mascara, documento) {
+            var i = documento.value.length;
+            var saida = mascara.substring(0, 1);
+            var texto = mascara.substring(i);
+
+            if (texto.substring(0, 1) != saida) {
+                documento.value += texto.substring(0, 1);
+            }
+
+        }
+
+        function handleChange(src) {
+            if (src.name == 'is_pastoral') {
+                var pastoral = document.querySelector("#pastoral");
+                if (src.value == 0) {
+                    pastoral.disabled = true;
+                    pastoral.value = '';
+                } else {
+                    pastoral.disabled = false;
+                }
+            }
+            if (src.name == 'is_married') {
+                var spouse_name = document.querySelector("#spouse_name");
+                var is_spouse_camper_yes = document.querySelector("#is_spouse_camper_yes");
+                var is_spouse_camper_no = document.querySelector("#is_spouse_camper_no");
+                if (src.value == 0) {
+                    spouse_name.disabled = true;
+                    spouse_name.value = '';
+                    is_spouse_camper_yes.disabled = true;
+                    is_spouse_camper_no.disabled = true;
+                    is_spouse_camper_yes.checked = false;
+                    is_spouse_camper_no.checked = false;
+                } else {
+                    spouse_name.disabled = false;
+                    is_spouse_camper_yes.disabled = false;
+                    is_spouse_camper_no.disabled = false;
+                }
             }
         }
-    @php } @endphp
+    </script>
 
-    function formatar(mascara, documento) {
-        var i = documento.value.length;
-        var saida = mascara.substring(0, 1);
-        var texto = mascara.substring(i);
-
-        if (texto.substring(0, 1) != saida) {
-            documento.value += texto.substring(0, 1);
-        }
-
-    }
-
-    function handleChange(src) {
-        if (src.name == 'is_pastoral') {
-            var pastoral = document.querySelector("#pastoral");
-            if (src.value == 0) {
-                pastoral.disabled = true;
-                pastoral.value = '';
-            } else {
-                pastoral.disabled = false;
-            }
-        }
-        if (src.name == 'is_married') {
-            var spouse_name = document.querySelector("#spouse_name");
-            var is_spouse_camper_yes = document.querySelector("#is_spouse_camper_yes");
-            var is_spouse_camper_no = document.querySelector("#is_spouse_camper_no");
-            if (src.value == 0) {
-                spouse_name.disabled = true;
-                spouse_name.value = '';
-                is_spouse_camper_yes.disabled = true;
-                is_spouse_camper_no.disabled = true;
-                is_spouse_camper_yes.checked = false;
-                is_spouse_camper_no.checked = false;
-            } else {
-                spouse_name.disabled = false;
-                is_spouse_camper_yes.disabled = false;
-                is_spouse_camper_no.disabled = false;
-            }
-        }
-    }
-</script>
+@endsection
